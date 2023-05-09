afade773a3c48e7fe2b7f6e28e90a131820010b3
Abdullah-M4 <131791479+Abdullah-M4@users.noreply.github.com>
2023-05-09 17:54:58 +0300

    Update delay.c

--- delay.c
+++ delay.c
@@ -1,25 +1,17 @@
 
-#include "micr.h"
-void SysTick_Init(void){
-NVIC_ST_CTRL_R &= ~(0x00000001); // disable SysTick during setup
- NVIC_ST_RELOAD_R = 0x00FFFFFF; // maximum reload value
- NVIC_ST_CURRENT_R = 0; // any write to current clears it
- NVIC_ST_CTRL_R = 0x00000005; // enable SysTick with core clock
-}
-
- 
-void SysTick_Wait(void){    /// wait 1 sec  for one time
- NVIC_ST_RELOAD_R = 16000000-1; // number of counts to wait
+void SysTick_Wait1m(void){   /// wait 1 milli sec for one time
+ NVIC_ST_RELOAD_R = 16000-1; // number of counts to wait
  NVIC_ST_CURRENT_R = 0; // any value written to CURRENT clears
  while((NVIC_ST_CTRL_R&0x00010000)==0){ // wait for count flag
  }
 }
 
-void SysTick_Wait1s(unsigned long delay1) /// wait (delay1) sec 
+
+void SysTick_Wait1ms(unsigned long delay1)  // wait (delay1) milli 
 {
  unsigned long i;
  for(i=0; i<delay1; i++)
 {
- SysTick_Wait(); // wait 1s (assumes 16 MHz clock)
+SysTick_Wait1m() ; // wait 1ms (assumes 16 MHz clock)
  }
 }
